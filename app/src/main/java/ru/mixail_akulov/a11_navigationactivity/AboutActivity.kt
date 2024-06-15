b9e1ba685d58128338d3b6015a59f4ddf4cc27af
AMixailV <Mixer-X@mail.ru>
2024-06-15 10:35:12 +0300

    Initial commit

---
+++ app/src/main/java/ru/mixail_akulov/a11_navigationactivity/AboutActivity.kt
@@ -0,0 +1,22 @@
+package ru.mixail_akulov.a11_navigationactivity
+
+import android.os.Bundle
+import ru.mixail_akulov.a11_navigationactivity.databinding.ActivityAboutBinding
+
+class AboutActivity : BaseActivity() {
+
+    private lateinit var binding: ActivityAboutBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityAboutBinding.inflate(layoutInflater).also { setContentView(it.root) }
+
+        binding.versionNameTextView.text = BuildConfig.VERSION_NAME
+        binding.versionCodeTextView.text = BuildConfig.VERSION_CODE.toString()
+        binding.okButton.setOnClickListener { onOkPressed() }
+    }
+
+    private fun onOkPressed() {
+        finish()
+    }
+}
